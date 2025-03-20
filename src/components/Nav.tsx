4ffe7b1353db2b120202878801cdb4227f370529
Donahue Avila <donahue.avila@gmail.com>
2025-03-20 13:47:35 -0500

    Update resume

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,7 +12,7 @@ export default function Nav() {
       <Link className={`pt-4 pb-2 px-4 hover:bg-slate-800    transition-color duration-200  ease-in ${path === '/' ? "underline" : undefined}`}  href="/"   >Home</Link>
       <Link className={`pt-4 pb-2 px-4 hover:bg-slate-800  transition-color duration-200  ease-in ${path.startsWith('/blog') ? "underline" : undefined}`}  href="/blog" >Blog</Link>
       <a className={`pt-4 pb-2 px-4 hover:bg-slate-800  transition-color duration-200  ease-in`} href="https://github.com/donahuea" >GitHub</a>
-      <a className={`pt-4 pb-2 px-4 hover:bg-slate-800  transition-color duration-200  ease-in`} href="/donahue_avila_resume.pdf">Resume</a>
+      <a className={`pt-4 pb-2 px-4 hover:bg-slate-800  transition-color duration-200  ease-in`} href="/donahue-avila-resume.pdf">Resume</a>
     </nav>
   </div>);
 }
